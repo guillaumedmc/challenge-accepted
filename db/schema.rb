69d8915171354156b0601ec93ecdbe944ef86a3b
Guillaume de Montecler <guillaume.demontecler@gmail.com>
2019-12-08 14:55:28 +0100

    ok

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_08_122107) do
+ActiveRecord::Schema.define(version: 2019_12_08_135232) do
 
   create_table "challenges", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,9 @@ ActiveRecord::Schema.define(version: 2019_12_08_122107) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
