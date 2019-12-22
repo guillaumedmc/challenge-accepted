b2088165ab1ee90f8e0aca0767f613d491809263
Guillaume de Montecler <guillaume.demontecler@gmail.com>
2019-12-22 23:07:43 +0100

    ok pour push

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,9 +11,19 @@ def create
     end
   end
 
+  # def show
+  #   @comment = Comment.find(params[:id])
+  # end
+
+  # def destroy
+  #   @comment = Comment.find(comment_params)
+  #   @comment.destroy
+  #   redirect_to challenge_path(@challenge), notice: 'Comment was successfully deleted.'
+  # end
+
   private
 
   def comment_params
-    params.require(:comment).permit(:content)
+    params.require(:comment).permit(:content, :id)
   end
 end
--- app/models/participant.rb
+++ app/models/participant.rb
@@ -1,4 +1,6 @@
 class Participant < ApplicationRecord
   belongs_to :user
   belongs_to :challenge
+  validates :role, presence: true
+  validates :user, presence: true, uniqueness: { scope: [:challenge, :role] }
 end
